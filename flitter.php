44e5ec64705e8ff718663fe896b91825291d97f1
carlo <littleark@gmail.com>
2010-02-10 23:52:15 +0100

    it looks like it works fine

--- flitter.php
+++ flitter.php
@@ -6,6 +6,7 @@ $twitter_request="http://search.twitter.com/search.json?q=the&rpp=50";
 $twitter_json = file_get_contents($twitter_request);
 
 //hmmmmmmmmmm
+//apparently I got it!
 
 //echo "<pre>";
 //var_dump(json_decode($twitter_json,true));
